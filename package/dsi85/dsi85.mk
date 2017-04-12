6bddd1a0b4dddc65389b8a00963ae9d5779c1a58
Konrad Anton <konrad.anton@awinia.de>
2017-04-12 20:17:23 +0200

    Make dsi85 a kernel extension package
    And stop dsi85 package depending on linux

--- package/dsi85/dsi85.mk
+++ package/dsi85/dsi85.mk
@@ -11,14 +11,14 @@ DSI85_VERSION = 241f587f
 DSI85_SITE = $(TOPDIR)/../dsi85
 DSI85_SITE_METHOD = local
 
-DSI85_DEPENDENCIES = linux
+# DSI85_DEPENDENCIES = linux
 
-define DSI85_BUILD_CMDS
-	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
-endef
+#define DSI85_BUILD_CMDS
+#	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
+#endef
 
-define DSI85_INSTALL_TARGET_CMDS
-	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
-endef
+#define DSI85_INSTALL_TARGET_CMDS
+#	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
+#endef
 
 $(eval $(generic-package))
